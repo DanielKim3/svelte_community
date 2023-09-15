bca9042e3abde4ebc421a1110cc803e72704f619
kimwoojin <wjinkim21@naver.com>
2023-09-15 23:49:59 +0900

    메인 페이지 컨텐츠, 로그인 레이아웃 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
-<h1>Main</h1>
-
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab blanditiis numquam corrupti odit vero
-dolorem pariatur tenetur voluptates? Error quasi exercitationem qui, repellat id culpa dolorum aut
-quidem tempora consectetur.
+<div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Welcom to 스콜</h1>
+        <p class="py-6">스콜은 2024년 서비스 예정인 IT 커뮤니티 사이트 입니다. 개발, 디자인, 기획, 스터디 그리고 취업준비 까지 여기서 한번에 해결하세요. <i>Bon appetit!</i></p>
+        <button class="btn btn-primary">시작하기</button>
+      </div>
+    </div>
+  </div>
--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -10,13 +10,13 @@
   </div> 
   <div>
       <a href="/dev" class="btn btn-ghost rounded-btn">개발 커뮤니티</a>
-      <a href="/designer" class="btn btn-ghost rounded-btn">디자이너 커뮤니티</a>
+      <a href="/designer" class="btn btn-ghost rounded-btn">디자인 커뮤니티</a>
       <a href="/pm" class="btn btn-ghost rounded-btn">기획 커뮤니티</a>
       <a href="/study" class="btn btn-ghost rounded-btn">스터디 커뮤니티</a>
       <a href="/job-seeker" class="btn btn-ghost rounded-btn">취준생 커뮤니티</a>
   </div>
   <div  >
-    <a href="#top" class="btn btn-ghost rounded-btn">로그인</a>
+    <a href="/login" class="btn btn-ghost rounded-btn">로그인</a>
     <a href="#top" class="btn btn-ghost rounded-btn">회원가입</a>
 </div>
 </div>
