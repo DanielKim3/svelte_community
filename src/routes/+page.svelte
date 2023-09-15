a20e07d9ff9df59ac33f46c8cddb1fe457aabca6
kimwoojin <wjinkim21@naver.com>
2023-09-16 00:00:48 +0900

    컨텐츠 수정

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="hero min-h-screen bg-base-200">
     <div class="hero-content text-center">
       <div class="max-w-md">
-        <h1 class="text-5xl font-bold">Welcom to 스콜</h1>
+        <h1 class="text-5xl font-bold">스콜에 오신것을 <br> 환영합니다!</h1>
         <p class="py-6">스콜은 2024년 서비스 예정인 IT 커뮤니티 사이트 입니다. 개발, 디자인, 기획, 스터디 그리고 취업준비 까지 여기서 한번에 해결하세요. <i>Bon appetit!</i></p>
         <button class="btn btn-primary">시작하기</button>
       </div>
